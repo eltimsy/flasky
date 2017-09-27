1c4b528a4dc81feba394b7eb0d39c30f74d81013
eltimsy <timothy.sc.tse@gmail.com>
2017-09-27 11:41:30 -0400

    added user table to db

--- flasky/schema.sql
+++ flasky/schema.sql
@@ -9,4 +9,10 @@ create table beer (
   name text not null,
   url text not null,
   description text,
+);
+create table user (
+  id integer primary key autoincrement,
+  name text not null,
+  password text not null,
+  email text,
 )
